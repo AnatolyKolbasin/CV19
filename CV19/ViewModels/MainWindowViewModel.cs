9497edee8629b8c791b71d0d26932762ab73876f
qwnize@outlook.com <qwnize@outlook.com>
2021-01-23 18:52:45 +0300

    bind status

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -17,7 +17,20 @@ namespace CV19.ViewModels
         {
             get => _Title;
             set => Set(ref _Title, value);
-        } 
+        }
+        #endregion
+
+        #region Status
+        private string _Status = "Ready!";
+
+        /// <summary>
+        /// Status
+        /// </summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
         #endregion
     }
 }
