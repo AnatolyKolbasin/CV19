bed00945f03c64733b429e9a5a1e9abf5851e205
qwnize@outlook.com <qwnize@outlook.com>
2021-01-23 15:49:34 +0300

    delete trash

--- Tests/CV19WPFTest/App.xaml.cs
+++ Tests/CV19WPFTest/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace CV19WPFTest
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
