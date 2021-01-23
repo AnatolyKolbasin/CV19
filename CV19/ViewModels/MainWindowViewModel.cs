e76cd0920698f522ad859d459919e5942da8dbea
qwnize@outlook.com <qwnize@outlook.com>
2021-01-23 17:14:06 +0300

    Test view-model work

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Window Title
+        private string _Title = "Statistical analysis CV19";
+
+        /// <summary>
+        /// Window Title
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        } 
+        #endregion
+    }
+}
