53dd8f2aef86a7558f93ebc25d96b558d472c6b4
qwnize@outlook.com <qwnize@outlook.com>
2021-01-25 22:07:31 +0300

    add models

---
+++ CV19/Models/CountryInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.Models
+{
+    internal class CountryInfo : PlaceInfo
+    {
+        public IEnumerable<ProvinceInfo> ProvinceCounts { get; set; }
+    }
+}
